229d7eda83f507ff9233809b1fe83e23c556a5ee
Anjali Sharma <sharma.anjali1498@gmail.com>
2020-05-02 03:24:55 +0530

    Updates

---
+++ precache-manifest.255b05eee1d91fc4b27e088ace472241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6605529aa83c290333f7fa40e31d98d3",
+    "url": "/Personal-Advisor-React-App/index.html"
+  },
+  {
+    "revision": "69cb69f80d4445daf107",
+    "url": "/Personal-Advisor-React-App/static/css/main.35f0bc35.chunk.css"
+  },
+  {
+    "revision": "024086cfb248cdd62850",
+    "url": "/Personal-Advisor-React-App/static/js/2.df724e71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Advisor-React-App/static/js/2.df724e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cb69f80d4445daf107",
+    "url": "/Personal-Advisor-React-App/static/js/main.126c001c.chunk.js"
+  },
+  {
+    "revision": "a90f4f6aab49943a8582",
+    "url": "/Personal-Advisor-React-App/static/js/runtime-main.cb5c69f0.js"
+  }
+]);
